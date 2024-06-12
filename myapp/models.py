4501f5649399a759233f1bac6192ecaeb6ed1309
rakeshshah18 <21amtics464@gmail.com>
2024-06-12 19:51:41 +0530

    learned makemigration and migrate models to database

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Menuitems(models.Model):
+    name = models.CharField(max_length=200)
+    course = models.CharField(max_length=300)
+    year = models.IntegerField()
+    
