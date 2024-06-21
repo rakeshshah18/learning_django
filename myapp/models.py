863511b53803013a98e569b072788a762c43eb30
rakeshshah18 <21amtics464@gmail.com>
2024-06-22 01:04:11 +0530

    created models only

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class MenuCategory(models.Model):
+    menu_category_name = models.CharField(max_length=200)
+    
+    
+class Menu(models.Model):
+    menu_item = models.CharField(max_length=200)
+    price = models.IntegerField(null=False)
+    category_id = models.ForeignKey(MenuCategory, on_delete=models.PROTECT, default=None, related_name="category_name")
+        
+        
+#ORM
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    reservation_day = models.CharField(max_length=20)
+    seats = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
