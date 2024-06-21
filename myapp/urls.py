7d8b243b8484e70df4eb8b0a612efb3842afa186
rakeshshah18 <21amtics464@gmail.com>
2024-06-22 02:35:09 +0530

    form rendering using templates

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('homepage/', views.homepage),
-    path('display_dat/', views.display_date),
-    
+    path('', views.home, name='home'),
+    path('homepage/', views.homepage, name='homepage'),
+    path('display_date/', views.display_date, name='display_date'),
+    path('display_forms/', views.display_forms, name='display_forms'),
 ]
