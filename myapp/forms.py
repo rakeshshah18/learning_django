94f607c30d960edf07cfb30b974e37aebbdee55b
rakeshshah18 <21amtics464@gmail.com>
2024-06-27 01:06:37 +0530

    temp commit

--- myapp/forms.py
+++ myapp/forms.py
@@ -8,7 +8,7 @@ shifts = [
 ]
 
 class InputForm(forms.Form):
-    first_name = forms.CharField(max_length=200)
-    last_name = forms.CharField(max_length=200)
+    first_name = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'placeholder':'first name'}))
+    last_name = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'placeholder':'last name'}))
     shifts = forms.ChoiceField(choices=shifts)
-    time_log = forms.TimeField()
+    time_log = forms.TimeField( widget=forms.TimeInput(attrs={'placeholder':'time log'}), help_text="Enter exact time")
