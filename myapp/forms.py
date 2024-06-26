dcf6d4a8f6b65e7670562140a83dc9c1f3deb050
rakeshshah18 <21amtics464@gmail.com>
2024-06-26 19:59:31 +0530

    temp commit

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,9 +1,21 @@
 from django import forms
 from django.forms.widgets import NumberInput
 
+favorite_dish = [
+    ('', 'Select your favorite dish'),
+    ('Pizza', 'Pizza'),
+    ('Burger', 'Burger'),
+    ('Pasta', 'Pasta'),
+    ('Salad', 'Salad'),
+    ('Ice Cream', 'Ice Cream'),
+    ('Others', 'Others')
+
+]
+
 class DemoForm(forms.Form):
     name = forms.CharField(max_length=100)
     age = forms.IntegerField(label='Your age')
     feedback = forms.CharField(widget=forms.Textarea(attrs={'rows': 3, 'cols': 30}))
-
     date = forms.DateField(widget=NumberInput(attrs={'type': 'date'}))
+    dish = forms.ChoiceField(widget=forms.RadioSelect, choices=favorite_dish)
+    # if we remove the widget=forms.RadioSelect, the choices will be displayed as a dropdown menu
