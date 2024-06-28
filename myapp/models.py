30181a3518dd260cb95690b3b6fd91ac5316c6d3
rakeshshah18 <21amtics464@gmail.com>
2024-06-28 11:18:18 +0530

    added admin reservation model

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Reservation(models.Model):
+    name = models.CharField(max_length=100, blank=True)
+    contact = models.CharField('phone number', max_length=100)
+    time = models.TimeField()
+    count = models.IntegerField()
+    notes = models.TextField(blank=True, max_length=1000)
+    
+def __str__(self):
+    return self.name
