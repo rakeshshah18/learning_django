fc173000b7f6b7c24a96e7709e7b443cc8d309a5
rakeshshah18 <21amtics464@gmail.com>
2024-07-03 01:58:03 +0530

    learned use of templates and To show/use dynamic data

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
     path('homepage/', views.homepage),
-    path('display_dat/', views.display_date),
-    
+    path('menu/', views.menu),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from datetime import datetime
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse('Hello, Django!')
 
 def homepage(request):
     return HttpResponse("Welcome to the Django homepage!")
 
-def display_date(request):
-    return HttpResponse("This page was served at " + str(datetime.today().year))
+def menu(request):
+    mainmenu = {'mains': [
+        {'name': "rakesh", 'age': "25"},
+        {'name': "suresh", 'age': "30"},
+        {'name': "mahesh", 'age': "35"},
+    ]}
+    return render(request, "menu.html", mainmenu)
